2c1668eb55226f1bab10cae71177b423616c36c3
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-01-23 13:57:30 +0100

    Implemented stripe messages and some debugging

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -57,11 +57,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (
-    await User.findOne({
-      email: session.customer_email,
-    })
-  ).id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
@@ -83,9 +79,7 @@ exports.webhookCheckout = (req, res, next) => {
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
-  res.status(200).json({
-    received: true,
-  });
+  res.status(200).json({ received: true });
 };
 
 exports.createBooking = factory.createOne(Booking);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingCtrl = require('../controllers/bookingController');
 
 const router = express.Router();
+
+router.use(viewsController.alerts);
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
@@ -12,12 +13,7 @@ router.get('/signup', viewsController.getSignupForm);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
 
-router.get(
-  '/my-tours',
-  // bookingCtrl.createBookingCheckout,
-  authController.protect,
-  viewsController.getMyTours,
-);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
